f85579a413491f42805a5ad48d0ee300d8d9745c
Daniel Biundo <danielbiundo@daniels-air.bc.edu>
2025-02-16 13:49:17 -0500

    Vercel

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,8 +1,17 @@
 const express = require('express');
 const router = express.Router();
-const { healthCheck, helloWorld } = require('../controllers');
+const { 
+  healthCheck, 
+  helloWorld, 
+  checkTable, 
+  refreshBills, 
+  submitBill 
+} = require('../controllers');
 
 router.get('/health', healthCheck);
 router.get('/hello', helloWorld);
+router.get('/table/:tableName/exists', checkTable);
+router.get('/bills/refresh', refreshBills);
+router.post('/submit-bill', submitBill);
 
 module.exports = router; 
